686c0798c5b399de5c14428bb39449791811cec1
Samantha Aleman <samantha.aleman25@gmail.com>
2020-07-29 01:00:33 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
-def countdown(counter)
-  while counter > 0
-    puts "#{counter} SECOND(S)!"
-    counter -= 1
+def countdown
+def countdown(x)
+  while x > 0
+    puts "#{x} SECOND(S)!"
+    x -= 1
   end
   "HAPPY NEW YEAR!"
 end
-
-def countdown_with_sleep(counter)
-    while counter > 0
-      puts "#{counter} SECOND(S)!"
-      sleep(1)
-      counter -= 1
+def countdown_with_sleep(x)
+  while x > 0
+      puts "#{x} SECOND(S)!"
+  sleep(1)
+    x -= 1
     end
     "HAPPY NEW YEAR!"
-  end
+end
